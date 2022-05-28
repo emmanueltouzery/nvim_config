aee1985a95815a634efdd913458f70a5529ca344
Emmanuel Touzery <etouzery@gmail.com>
2022-05-28 20:00:06 +0200

    nvim-tree: don't change the current folder+hide .git

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -7,16 +7,8 @@ require'nvim-tree'.setup {
     enable = true,
   },
   remove_file_close_window = false,
-  update_cwd = true,
   update_focused_file = {
-    -- enables the feature
     enable = true,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd = true,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = {},
   },
   view = {
     mappings = {
@@ -75,6 +67,9 @@ require'nvim-tree'.setup {
         chars = '234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ',
       }
     }
+  },
+  filters = {
+    custom = { ".git" }, -- ignore .git
   },
 }
 

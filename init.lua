711f737dd45b59e0f38efdd77fc69b246a7d64e2
Emmanuel Touzery <etouzery@gmail.com>
2022-05-30 17:07:40 +0200

    Revert "remove luasnip mentions in the completion stuff"
    This reverts commit 628a364a943a505c8d75bff7ad358fad0c3ed38c.

--- init.lua
+++ init.lua
@@ -411,6 +411,11 @@ require('nvim-treesitter.configs').setup {
 -- nvim-cmp setup
 local cmp = require 'cmp'
 cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -422,6 +427,8 @@ cmp.setup {
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -429,6 +436,8 @@ cmp.setup {
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
       else
         fallback()
       end
@@ -436,6 +445,7 @@ cmp.setup {
   }),
   sources = {
     { name = 'nvim_lsp' },
+    { name = 'luasnip' },
   },
 }
 

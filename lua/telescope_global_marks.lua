e81b644d1225b72a53869d975c8f0f35ba92e1a4
Emmanuel Touzery <etouzery@gmail.com>
2022-05-31 23:38:12 +0200

    telescope global marks: ability to delete marks

--- lua/telescope_global_marks.lua
+++ lua/telescope_global_marks.lua
@@ -1,4 +1,5 @@
 local pickers = require "telescope.pickers"
+local action_state = require "telescope.actions.state"
 local utils = require "telescope.utils"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
@@ -9,55 +10,73 @@ local Str = require'plenary.strings'
 local Path = require'plenary.path'
 
 _G.telescope_global_marks = function(opts)
-  local local_marks = {
-    items = vim.fn.getmarklist(opts.bufnr),
-    name_func = function(_, line)
-      return vim.api.nvim_buf_get_lines(opts.bufnr, line - 1, line, false)[1]
-    end,
-  }
-  local global_marks = {
-    items = vim.fn.getmarklist(),
-    name_func = function(mark, _)
-      -- get buffer name if it is opened, otherwise get file name
-      return vim.api.nvim_get_mark(mark, {})[4]
-    end,
-  }
-  local marks_table = {}
-  local bufname = vim.api.nvim_buf_get_name(opts.bufnr or 0)
-  for _, cnf in ipairs { local_marks, global_marks } do
-    for _, v in ipairs(cnf.items) do
-      -- strip the first single quote character
-      local mark = string.sub(v.mark, 2, 3)
-      local _, lnum, col, _ = unpack(v.pos)
-      -- need to use plenary to expand the path, else when the buffer is
-      -- opened, I get ~ for the start of the path and I can't find the project.
-      local name = Path.new(cnf.name_func(mark, lnum)):expand()
-      local path_project = to_file_path_in_project(name)
-      local row = {
-        mark = mark,
-        project = path_project and path_project[1]:match("[^/]+$") or "-",
-        relative_fname = path_project and path_project[2] or name,
-        lnum = lnum,
-        col = col,
-        filename = v.file or bufname,
-      }
-      -- only keep global marks (u="uppercase")
-      if mark:match "%u" then
-        table.insert(marks_table, row)
+  local get_marks_table = function()
+    local global_marks = {
+      items = vim.fn.getmarklist(),
+      name_func = function(mark, _)
+        -- get buffer name if it is opened, otherwise get file name
+        return vim.api.nvim_get_mark(mark, {})[4]
+      end,
+    }
+    local marks_table = {}
+    local bufname = vim.api.nvim_buf_get_name(opts.bufnr or 0)
+    for _, cnf in ipairs { global_marks } do
+      for _, v in ipairs(cnf.items) do
+        -- strip the first single quote character
+        local mark = string.sub(v.mark, 2, 3)
+        local _, lnum, col, _ = unpack(v.pos)
+        -- need to use plenary to expand the path, else when the buffer is
+        -- opened, I get ~ for the start of the path and I can't find the project.
+        local name = Path.new(cnf.name_func(mark, lnum)):expand()
+        local path_project = to_file_path_in_project(name)
+        local row = {
+          mark = mark,
+          project = path_project and path_project[1]:match("[^/]+$") or "-",
+          relative_fname = path_project and path_project[2] or name,
+          lnum = lnum,
+          col = col,
+          filename = v.file or bufname,
+        }
+        -- only keep global marks (u="uppercase")
+        if mark:match "%u" then
+          table.insert(marks_table, row)
+        end
       end
     end
+    return marks_table
+  end
+
+  local gen_new_finder = function()
+    return finders.new_table {
+      results = get_marks_table(),
+      entry_maker = global_marks_entry_maker(),
+    }
+  end
+
+  local actions = {}
+  actions.delete_mark = function(prompt_bufnr)
+    local current_picker = action_state.get_current_picker(prompt_bufnr) -- picker state
+    local entry = action_state.get_selected_entry()
+    vim.cmd("delmarks " .. entry.mark)
+    -- https://github.com/neovim/neovim/issues/7198#issuecomment-323649157
+    -- without wshada! the marks would return after a nvim restart...
+    -- also slightly related: https://stackoverflow.com/a/32138657/516188
+    vim.cmd("wshada!")
+    current_picker:refresh(gen_new_finder(), { reset_prompt = true })
   end
 
   pickers.new(opts, {
     prompt_title = "Global Marks",
-    finder = finders.new_table {
-      results = marks_table,
-      entry_maker = global_marks_entry_maker(),
-    },
+    finder = gen_new_finder(),
     previewer = conf.grep_previewer(opts),
     sorter = conf.generic_sorter(opts),
     push_cursor_on_edit = true,
     push_tagstack_on_edit = true,
+    attach_mappings = function(_, map)
+      map("i", "<c-d>", actions.delete_mark)
+      map("n", "<c-d>", actions.delete_mark)
+      return true
+    end,
   }):find()
 end
 

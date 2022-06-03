e014b3fe0e92567b1d3f780c4e23eb163b68a7b6
Emmanuel Touzery <etouzery@gmail.com>
2022-06-03 17:56:42 +0200

    format on save also ts+js

--- init.lua
+++ init.lua
@@ -456,6 +456,8 @@ vim.cmd [[autocmd BufWritePre *.exs lua vim.lsp.buf.formatting_sync()]]
 vim.cmd [[autocmd BufWritePre *.rs lua vim.lsp.buf.formatting_sync()]]
 vim.cmd [[autocmd BufWritePre *.tsx lua vim.lsp.buf.formatting_sync()]]
 vim.cmd [[autocmd BufWritePre *.jsx lua vim.lsp.buf.formatting_sync()]]
+vim.cmd [[autocmd BufWritePre *.ts lua vim.lsp.buf.formatting_sync()]]
+vim.cmd [[autocmd BufWritePre *.js lua vim.lsp.buf.formatting_sync()]]
 
 vim.diagnostic.config({
   virtual_text = false,

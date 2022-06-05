9989e3a903843121447a534a70acd8760a7f76cc
Emmanuel Touzery <etouzery@gmail.com>
2022-06-05 16:02:55 +0200

    leave timeoutlen alone, i was REDUCING it compared to the defaults

--- init.lua
+++ init.lua
@@ -444,7 +444,7 @@ vim.opt.fillchars = vim.opt.fillchars + 'diff:╱,stl: '
 vim.o.relativenumber = true
 vim.opt.cursorline = true -- highlight the current line number
 vim.opt.clipboard = "unnamedplus"
-vim.opt.timeoutlen = 500 -- allow considerably more time to enter shortcuts
+-- vim.opt.timeoutlen = 500 -- allow considerably more time to enter shortcuts
 
 require("plugins.lualine")
 require("plugins.cmp")

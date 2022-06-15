53a6b308529df63f9b5cbaa5d72a66c733d26832
Emmanuel Touzery <etouzery@gmail.com>
2022-06-15 17:10:42 +0200

    use null-ls/prettier to indent markdown files

--- init.lua
+++ init.lua
@@ -470,6 +470,7 @@ vim.cmd [[autocmd BufWritePre *.tsx lua vim.lsp.buf.formatting_sync()]]
 vim.cmd [[autocmd BufWritePre *.jsx lua vim.lsp.buf.formatting_sync()]]
 vim.cmd [[autocmd BufWritePre *.ts lua vim.lsp.buf.formatting_sync()]]
 vim.cmd [[autocmd BufWritePre *.js lua vim.lsp.buf.formatting_sync()]]
+vim.cmd [[autocmd BufWritePre *.md lua vim.lsp.buf.formatting_sync()]]
 
 vim.diagnostic.config({
   virtual_text = false,

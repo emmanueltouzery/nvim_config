cf1ca82e0507d0c97d29e2daaa7debc31e2f96a8
Emmanuel Touzery <etouzery@gmail.com>
2022-06-15 20:01:30 +0200

    workaround a completion issue: https://github.com/hrsh7th/cmp-nvim-lsp-signature-help/issues/13

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -47,10 +47,21 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
+    -- ['<CR>'] = cmp.mapping.confirm {
+    --   behavior = cmp.ConfirmBehavior.Replace,
+    --   select = true,
+    -- },
+    ['<CR>'] = cmp.mapping(function(fallback) 
+      -- workaround for https://github.com/hrsh7th/cmp-nvim-lsp-signature-help/issues/13
+      if cmp.get_selected_entry().source.name == 'nvim_lsp_signature_help' then
+        fallback()
+      else
+        cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }(fallback)
+      end
+    end),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()

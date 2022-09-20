74d98d99a2babb9b1d68dbdbf074cfe5575d5e04
Emmanuel Touzery <etouzery@gmail.com>
2022-09-20 19:05:17 +0200

    upgrade agitator, little fix from me

--- init.lua
+++ init.lua
@@ -157,7 +157,7 @@ require('packer').startup(function(use)
     end
   }
   use {'nvim-telescope/telescope-live-grep-raw.nvim', commit='8124094e11b54a1853c3306d78e6ca9a8d40d0cb'}
-  use {'emmanueltouzery/agitator.nvim', commit='0cd057ed571c025ad110db06e22bb4df82fae84c'}
+  use {'emmanueltouzery/agitator.nvim', commit='42a15a4ea9bc7c6b809cb235719b2b8e928f448b'}
   use {'nvim-telescope/telescope-project.nvim', commit='8cd22b696e14b353fe8ea9648a03364cb56c39d4'}
   -- vim.cmd("let g:yankstack_yank_keys = ['c', 'C', 'd', 'D', 's', 'S', 'x', 'X', 'y', 'Y']")
   -- drop s and S due to lightspeed

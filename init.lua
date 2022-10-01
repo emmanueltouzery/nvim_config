e81b328fde9e7a5edd1253984a2fc93d0980dbcf
Emmanuel Touzery <etouzery@gmail.com>
2022-10-01 17:34:01 +0200

    migrate to my fork of key-menu with a fix

--- init.lua
+++ init.lua
@@ -315,7 +315,7 @@ callbacks = {
     end,
     after = "nvim-lspconfig",
   }
-  use {'linty-org/key-menu.nvim', commit='14afd14d73488118c1cf8419e01f9353351f7bdb'}
+  use {'emmanueltouzery/key-menu.nvim', commit='99ec37f4fa44060f23409a72303dbff39da2930e'}
   use {'lambdalisue/suda.vim', commit='6bffe36862faa601d2de7e54f6e85c1435e832d0'}
   use {'akinsho/toggleterm.nvim', commit='2a787c426ef00cb3488c11b14f5dcf892bbd0bda', config = function()
     require("toggleterm").setup{

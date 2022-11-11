22fd1c39c3463ec6c64ab66243d7699c82a373ca
Emmanuel Touzery <etouzery@gmail.com>
2022-11-11 21:36:36 +0100

    code actions: show null-ls last

--- init.lua
+++ init.lua
@@ -262,7 +262,45 @@ callbacks = {
             }
           end
         end
-      }
+      },
+      select = {
+        get_config = function(opts)
+          -- https://github.com/stevearc/dressing.nvim/issues/22#issuecomment-1067211863
+          -- for codeaction, we want null-ls to be last
+          -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/630
+          -- for eslint, it's offering me options like "disable eslint rule" which
+          -- are almost never what I want, and they appear before the more useful options
+          -- from the LSP
+          if opts.kind == 'codeaction' then
+            return {
+              telescope = {
+                sorter = require'telescope.sorters'.Sorter:new {
+                  scoring_function = function(_, _, line)
+                    if string.match(line, 'null-ls') then
+                      return 1
+                    else
+                      return 0
+                    end
+                  end,
+                },
+                -- copied from the telescope dropdown theme
+                sorting_strategy = "ascending",
+                layout_strategy = "center",
+                layout_config = {
+                  preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+                  width = 80,
+                  height = 15,
+                },
+                borderchars = {
+                  prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+                  results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+                  preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                },
+              }
+            }
+          end
+        end,
+      },
     })
   end}
   use {

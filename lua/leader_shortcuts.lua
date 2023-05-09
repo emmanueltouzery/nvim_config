687323f5c67f03695917b44f645873cd9371ede4
Emmanuel Touzery <etouzery@gmail.com>
2023-05-09 21:52:41 +0200

    spc-ft add config file type

--- lua/leader_shortcuts.lua
+++ lua/leader_shortcuts.lua
@@ -66,7 +66,7 @@ vim.keymap.set("n", "<leader>fdc", ":lua open_file_cur_dir(true)<cr>", {desc="op
 vim.keymap.set('n', '<leader>f!', ":windo e! | windo Gitsigns refresh<cr>", {desc = "Reload all files from disk"})
 
 function _G.quick_set_ft()
-  local filetypes = {"typescript", "json", "elixir", "rust", "lua", "diff", "sh", "markdown", "html", "other"}
+  local filetypes = {"typescript", "json", "elixir", "rust", "lua", "diff", "sh", "markdown", "html", "config", "other"}
   vim.ui.select(filetypes, {prompt="Pick filetype to switch to"}, function(choice)
     if choice == "other" then
       vim.ui.input({prompt="Enter filetype", kind="center_win"}, function(word)

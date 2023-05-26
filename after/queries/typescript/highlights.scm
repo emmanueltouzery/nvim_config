50d5b2012816fbe3f9cb529664300a76567f1bb9
Emmanuel Touzery <etouzery@gmail.com>
2023-05-26 08:30:19 +0200

    spell-check: also accept rgba color specs

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -2,4 +2,4 @@
 
 ; don't spell-check html color specs
 (string
-      (string_fragment) @name @string (#match? @string "^#[0-9a-f]{6}$")) @nospell
+      (string_fragment) @name @string (#match? @string "^#[0-9a-f]{6,8}$")) @nospell

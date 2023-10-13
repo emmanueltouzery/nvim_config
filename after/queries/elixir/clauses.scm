264f95cc45b27bfea16a6c3ff01d84ca9ad8c01d
Emmanuel Touzery <etouzery@gmail.com>
2023-10-13 23:27:06 +0200

    moved clauses.scm to the elixir-extras plugin

--- after/queries/elixir/clauses.scm
+++
@@ -1,8 +0,0 @@
-(call
-  target: (identifier) @identifier (#any-of? @identifier "def" "defp")
-  (arguments [
-              (call target: (identifier) @name)
-              (binary_operator left: (call target: (identifier) @name))
-   ])
-  ) @type
-

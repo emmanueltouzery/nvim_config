ef8a22c7e3d67c47a996a9c27ccde455dfd6a053
Emmanuel Touzery <etouzery@gmail.com>
2023-11-27 20:37:53 +0100

    fix nvim-tree fork commit

--- init.lua
+++ init.lua
@@ -700,7 +700,7 @@ callbacks = {
   end}
   use {
     -- hopefully temporarily using my fork due to https://github.com/stevearc/stickybuf.nvim/issues/24
-    'emmanueltouzery/nvim-tree.lua', commit='f2d8ce872bb9d1cdff4aa496d35628509da2f7b5',
+    'emmanueltouzery/nvim-tree.lua', commit='0a9ae9d40c2e03819706eccc717389f563ffe0ac',
     requires = { 'nvim-tree/nvim-web-devicons', commit='9ab9b0b894b2388a9dbcdee5f00ce72e25d85bf9' },
     -- for some reason must call init outside of the config block, elsewhere
     -- config = function() require'nvim-tree'.setup {} end

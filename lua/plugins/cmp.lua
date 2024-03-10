62e9f7b81fccc02e214306b5fab44684b029f4ea
Emmanuel Touzery <etouzery@gmail.com>
2024-03-10 15:15:48 +0100

    completions: all visible buffers+non-ascii

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -113,7 +113,22 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = "path" },
-    { name = "buffer" },
+    {
+      name = 'buffer',
+      option = {
+        -- fetch from all visible windows (not sure about multi-tab)
+        -- https://github.com/hrsh7th/cmp-buffer?tab=readme-ov-file#get_bufnrs-type-fun-number
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
+          end
+          return vim.tbl_keys(bufs)
+        end,
+        -- i think it's needed for non-ascii https://github.com/hrsh7th/cmp-buffer/issues/11
+        keyword_pattern = [[\k\+]],
+      }
+    },
     { name = 'nvim_lsp_signature_help' },
     { name = 'emoji', max_item_count = 3 },
   },

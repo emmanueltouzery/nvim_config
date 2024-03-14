015f6286e24c96132b83eedfb261f3699c06ca01
Emmanuel Touzery <etouzery@gmail.com>
2024-03-14 08:36:40 +0100

    new completion shortcut, C-e to abort

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -47,6 +47,7 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
     -- ['<CR>'] = cmp.mapping.confirm {
     --   behavior = cmp.ConfirmBehavior.Replace,
     --   select = true,

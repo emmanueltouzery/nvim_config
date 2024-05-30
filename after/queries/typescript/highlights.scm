c710ec5bfbd7fd32373140fe48e6b14144e312c9
Emmanuel Touzery <etouzery@gmail.com>
2024-05-30 22:22:01 +0200

    experimental typescript type concealing

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -3,3 +3,11 @@
 ; don't spell-check html color specs
 (string
       (string_fragment) @name @string (#match? @string "^#[0-9a-f]{6,8}$")) @nospell
+
+; experimental types concealing
+((type_alias_declaration) @conceal (#set! conceal ""))
+((type_annotation) @conceal (#set! conceal ""))
+((predefined_type) @conceal (#set! conceal ""))
+((type_parameters) @conceal (#set! conceal ""))
+((type_identifier) @conceal (#set! conceal ""))
+((as_expression "as" @conceal) (#set! conceal ""))

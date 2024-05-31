419133af77ecb14fed39b5a3be71ff959639c218
Emmanuel Touzery <etouzery@gmail.com>
2024-05-31 22:13:55 +0200

    new diffview shortcut, open difftastic in a popup

--- init.lua
+++ init.lua
@@ -361,7 +361,33 @@ require('packer').startup(function(use)
                 vim.w.orig_width = nil
               end
             end, {desc = "Toggle expansion of file panel to fit"}
-            };
+            },
+            {"n", "<leader>cF", function()
+              local file_path = require'diffview.lib'.get_current_view().panel.cur_file.path
+              local left_commit = require'diffview.lib'.get_current_view().left.commit
+              local right_commit = require'diffview.lib'.get_current_view().right.commit
+              local cmd = "GIT_EXTERNAL_DIFF='difft --display side-by-side-show-both' git diff " .. left_commit .. ":" .. file_path .. " " ..
+                right_commit .. ":" ..  file_path
+
+              local popup_buf = vim.api.nvim_create_buf(false, true)
+              local width = vim.o.columns-6
+              local height = vim.o.lines-6
+              local win_opts = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                relative = "editor",
+                width = width,
+                height = height,
+                anchor = "NW",
+                row = 3,
+                col = 3,
+                noautocmd = true,
+              }
+              local popup_win = vim.api.nvim_open_win(popup_buf, true, win_opts)
+
+              vim.fn.termopen(cmd)
+            end, {desc= "Diff with difftastic"}}
           },
           file_history_panel = {
             {"n", "gf", diffview_gf,

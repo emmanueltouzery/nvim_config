9e7a6b9c3dac5fa4e48732ef11d9f692c7f985f6
Emmanuel Touzery <etouzery@gmail.com>
2024-06-04 20:44:33 +0200

    diffview support difftastic also from file history

--- init.lua
+++ init.lua
@@ -288,6 +288,30 @@ require('packer').startup(function(use)
   end} 
   use {'sindrets/diffview.nvim', commit='3afa6a053f680e9f1329c4a151db988a482306cd',
     config = function()
+      local function open_difftastic(file_path, left_commit, right_commit)
+        local cmd = "GIT_EXTERNAL_DIFF='difft --display side-by-side-show-both' git diff " .. left_commit .. ":" .. file_path .. " " ..
+        right_commit .. ":" ..  file_path
+
+        local popup_buf = vim.api.nvim_create_buf(false, true)
+        local width = vim.o.columns-6
+        local height = vim.o.lines-6
+        local win_opts = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          relative = "editor",
+          width = width,
+          height = height,
+          anchor = "NW",
+          row = 3,
+          col = 3,
+          noautocmd = true,
+        }
+        local popup_win = vim.api.nvim_open_win(popup_buf, true, win_opts)
+
+        vim.fn.termopen(cmd)
+      end
+
       require('diffview').setup {
         keymaps = {
           view = {
@@ -366,33 +390,19 @@ require('packer').startup(function(use)
               local file_path = require'diffview.lib'.get_current_view().panel.cur_file.path
               local left_commit = require'diffview.lib'.get_current_view().left.commit
               local right_commit = require'diffview.lib'.get_current_view().right.commit
-              local cmd = "GIT_EXTERNAL_DIFF='difft --display side-by-side-show-both' git diff " .. left_commit .. ":" .. file_path .. " " ..
-                right_commit .. ":" ..  file_path
-
-              local popup_buf = vim.api.nvim_create_buf(false, true)
-              local width = vim.o.columns-6
-              local height = vim.o.lines-6
-              local win_opts = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                relative = "editor",
-                width = width,
-                height = height,
-                anchor = "NW",
-                row = 3,
-                col = 3,
-                noautocmd = true,
-              }
-              local popup_win = vim.api.nvim_open_win(popup_buf, true, win_opts)
-
-              vim.fn.termopen(cmd)
+              open_difftastic(file_path, left_commit, right_commit)
             end, {desc= "Diff with difftastic"}}
           },
           file_history_panel = {
             {"n", "gf", diffview_gf,
               {desc = "Goto File"},
             },
+            {"n", "<leader>cF", function()
+              local file_path = require'diffview.lib'.get_current_view().cur_layout.b.file.path
+              local left_commit = require'diffview.lib'.get_current_view().cur_layout.a.file.rev.commit
+              local right_commit = require'diffview.lib'.get_current_view().cur_layout.b.file.rev.commit
+              open_difftastic(file_path, left_commit, right_commit)
+            end, {desc= "Diff with difftastic"}}
           },
         },
         file_history_panel = {

60b6a0a04cf8f8e9232f2d3c742d9c0c8ec381a8
Emmanuel Touzery <etouzery@gmail.com>
2024-06-05 14:03:33 +0200

    revert TS types concealing
    unfortunately it breaks TS LSP hover popups https://github.com/nvim-treesitter/nvim-treesitter/issues/2825#issuecomment-2149661010

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -4,11 +4,11 @@
 (string
       (string_fragment) @name @string (#match? @string "^#[0-9a-f]{6,8}$")) @nospell
 
-; experimental types concealing
-((type_alias_declaration) @conceal (#set! conceal ""))
-((type_annotation) @conceal (#set! conceal ""))
-((predefined_type) @conceal (#set! conceal ""))
-((type_parameters) @conceal (#set! conceal ""))
-((type_identifier) @conceal (#set! conceal ""))
-((type_arguments) @conceal (#set! conceal ""))
-((as_expression "as" @conceal) (#set! conceal ""))
+; ; experimental types concealing
+; ((type_alias_declaration) @conceal (#set! conceal ""))
+; ((type_annotation) @conceal (#set! conceal ""))
+; ((predefined_type) @conceal (#set! conceal ""))
+; ((type_parameters) @conceal (#set! conceal ""))
+; ((type_identifier) @conceal (#set! conceal ""))
+; ((type_arguments) @conceal (#set! conceal ""))
+; ((as_expression "as" @conceal) (#set! conceal ""))

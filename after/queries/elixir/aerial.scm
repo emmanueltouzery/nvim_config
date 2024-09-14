2add34306cd1eaec63c668b3be66f6cdb14dd946
Emmanuel Touzery <etouzery@gmail.com>
2024-09-14 10:46:34 +0200

    elixir TS: cleanup, 1 query instead of 3

--- after/queries/elixir/aerial.scm
+++ after/queries/elixir/aerial.scm
@@ -3,17 +3,5 @@
 (do_block
   (call
     target: (identifier) @identifier @name
-    (#eq? @identifier "query")) @symbol
-  (#set! "kind" "Function")) @symbol
-
-(do_block
-  (call
-    target: (identifier) @identifier @name
-    (#eq? @identifier "mutation")) @symbol
-  (#set! "kind" "Function")) @symbol
-
-(do_block
-  (call
-    target: (identifier) @identifier @name
-    (#eq? @identifier "subscription")) @symbol
+    (#any-of? @identifier "query" "mutation" "subscription")) @symbol
   (#set! "kind" "Function")) @symbol

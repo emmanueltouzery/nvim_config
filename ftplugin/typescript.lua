5ee488a87636b29613271f364634c85ede61ac31
Emmanuel Touzery <etouzery@gmail.com>
2024-11-27 21:18:55 +0100

    typescript, tool for exploratory type analysis

---
+++ ftplugin/typescript.lua
@@ -0,0 +1,53 @@
+local function extract_type()
+  local parser = require('nvim-treesitter.parsers').get_parser(0)
+  parser:parse()
+  local ts_node = parser:named_node_for_range({vim.fn.line('.')-1, vim.fn.col('.')-1, vim.fn.line('.')-1, vim.fn.col('.')-1})
+  local parent = ts_node
+  while parent do
+    local p = parent:parent()
+    if p == nil or p:type() == 'program' or p:type() == 'lexical_declaration' or p:type() == 'statement_block' or p:type() == 'required_parameter' then
+      goto done
+    end
+    parent = p
+  end
+  ::done::
+  if parent then
+    if parent:parent() and parent:parent():type() == 'required_parameter' then
+      -- this is a function parameter
+
+      -- find the parameter index
+      local param = parent:parent()
+      local param_index = 0
+      while param do
+        param = param:prev_named_sibling()
+        if param then
+          param_index = param_index + 1
+        end
+      end
+
+      -- find the function start location
+      local row_node = parent
+      while row_node do
+        local p = row_node:parent()
+        if p == nil or p:type() == 'program' or p:type() == 'lexical_declaration' then
+          goto done
+        end
+        row_node = p
+      end
+      ::done::
+      local row = row_node:start()
+      vim.api.nvim_buf_set_lines(0, row, row, false, {
+        "type T = Parameters<typeof " .. require'aerial'.get_location()[1].name .. ">[" .. param_index .. "];"})
+      vim.api.nvim_win_set_cursor(0, {row+1, 5}) -- 5="type >T<"
+    else
+      -- normal variable
+      local row = parent:start()
+      vim.api.nvim_buf_set_lines(0, row, row, false, {"type T = typeof " .. vim.fn.expand('<cword>') .. ";"})
+      vim.api.nvim_win_set_cursor(0, {row+1, 5}) -- 5="type >T<"
+    end
+  end
+end
+
+-- tool for exploratory type programming. Extract a type T that can be explored with the LSP hover, K
+vim.keymap.set('n', '<localleader>t', extract_type, { buffer = true, desc = "Extract type for K exploration" })
+
---
+++ ftplugin/typescriptreact.lua
@@ -0,0 +1,2 @@
+-- https://www.reddit.com/r/neovim/comments/ykpnob/comment/iuy3lea/
+vim.cmd.runtime({"ftplugin/typescript.lua",  bang = true })

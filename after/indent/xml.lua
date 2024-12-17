bedfd75a61e465be1f29a78074f1ccc0515689bd
Emmanuel Touzery <etouzery@gmail.com>
2024-12-18 00:17:25 +0100

    xml indenter: now it indents the way i want

--- after/indent/xml.lua
+++ after/indent/xml.lua
@@ -1,11 +1,18 @@
-local function get_node_elt(node)
+local function get_node_elt(node, this_line_only)
+  local start_line, _, _, _ = node:range()
   while node:parent():type() ~= "element" and node:type() ~= "Comment" do
+    if this_line_only then
+      local cur_start_line, _, _, _ = node:parent():range()
+      if cur_start_line ~= start_line then
+        return node
+      end
+    end
     node = node:parent()
   end
   return node
 end
 
-local function xml_indent_offset_for_lnum(parser, lnum, indent_map)
+local function xml_indent_offset_for_lnum(parser, lnum, mode, nodes_on_line_previous)
   local line_contents = vim.api.nvim_buf_get_lines(0, lnum, lnum+1, false) or {""}
   -- print(vim.inspect(line_contents))
   local m = string.match(line_contents[1], "^%s+")
@@ -15,7 +22,7 @@ local function xml_indent_offset_for_lnum(parser, lnum, indent_map)
   end
   -- print("first_char_offset for " .. line_contents[1] .. ": " .. first_char_offset)
 
-  local ts_node = get_node_elt(parser:named_node_for_range({lnum, first_char_offset, lnum, first_char_offset}))
+  local ts_node = get_node_elt(parser:named_node_for_range({lnum, first_char_offset, lnum, first_char_offset}), mode == "cur_line")
   -- find all the named nodes on that line
   local first_node = ts_node
   local _, first_col, _, _ = ts_node:range()
@@ -45,39 +52,52 @@ local function xml_indent_offset_for_lnum(parser, lnum, indent_map)
     table.insert(nodes_on_line, last_node)
   end
 
-  print("nodes on line: " .. #nodes_on_line)
+  -- print("nodes on line: " .. #nodes_on_line)
 
   local indent_level_change = 0
+  local had_stag = false
   for _, node in ipairs(nodes_on_line) do
-      print(node:type() .. "=>" .. vim.treesitter.get_node_text(node, 0))
     if node:type() ~= "content" then
-      indent_level_change = indent_level_change + (indent_map[node:type()] or 0)
+      -- print(node:type() .. "=>" .. vim.treesitter.get_node_text(node, 0))
+      if mode == "previous_line" then
+        if node:type() == "STag" then
+          indent_level_change = indent_level_change + vim.bo.shiftwidth
+        end
+        if node:type() == "ETag" and indent_level_change > 0 then
+          -- compensate the previous STag since it got closed
+          indent_level_change = indent_level_change - vim.bo.shiftwidth
+        end
+      elseif mode == "cur_line" then
+        if node:type() == "STag" then
+          had_stag = true
+        end
+        if node:type() == "ETag" then
+          if had_stag then
+            had_stag = false
+          else
+            indent_level_change = indent_level_change - vim.bo.shiftwidth
+          end
+        end
+        if node:type() == "Attribute" and nodes_on_line_previous[1]:type() ~= "Attribute" then
+          indent_level_change = indent_level_change + vim.bo.shiftwidth
+        end
+      end
     end
   end
 
-  return first_col, indent_level_change
+  return first_col, nodes_on_line, indent_level_change
 end
 
 function _G.xml_get_indent(lnum)
-  print("lnum: " .. (lnum or "nil"))
+  -- print("lnum: " .. (lnum or "nil"))
   local parser = require('nvim-treesitter.parsers').get_parser(0)
   parser:parse()
 
-  -- the previous line can only ADD offset
-  local first_col_previous, offset_previous = xml_indent_offset_for_lnum(parser, lnum-2, {
-    STag = vim.bo.shiftwidth,
-    ETag = -vim.bo.shiftwidth,
-  })
-  print("first_col_previous " .. first_col_previous)
-  local indent_offset_previous_line = math.max(0, offset_previous)
-  print("indent_offset_previous_line: " .. indent_offset_previous_line)
-  -- the current line can only SUBTRACT offset
-  local first_col_cur, offset_cur = xml_indent_offset_for_lnum(parser, lnum-1, {
-    STag = vim.bo.shiftwidth,
-    ETag = -vim.bo.shiftwidth,
-  })
-  local indent_offset_cur_line = math.min(0, offset_cur)
-  print("indent_offset_cur_line: " .. indent_offset_cur_line)
+  local first_col_previous, nodes_on_line_previous, indent_offset_previous_line = xml_indent_offset_for_lnum(parser, lnum-2, "previous_line")
+  -- print("first_col_previous " .. first_col_previous)
+  -- print("indent_offset_previous_line: " .. indent_offset_previous_line)
+  local first_col_cur, _, indent_offset_cur_line = xml_indent_offset_for_lnum(parser, lnum-1, "cur_line", nodes_on_line_previous)
+  -- print("indent_offset_cur_line: " .. indent_offset_cur_line)
 
   -- local previous_line = vim.api.nvim_buf_get_lines(0, lnum-2,lnum-1, false) or {""}
   -- print("previous_line: " .. vim.inspect(previous_line))
@@ -87,7 +107,7 @@ function _G.xml_get_indent(lnum)
   --   previous_indent = #m
   -- end
   -- print("previous_indent: " .. previous_indent)
-  print("res " .. tostring(first_col_previous + indent_offset_previous_line + indent_offset_cur_line))
+  -- print("res " .. tostring(first_col_previous + indent_offset_previous_line + indent_offset_cur_line))
   return tostring(first_col_previous + indent_offset_previous_line + indent_offset_cur_line)
 end
 

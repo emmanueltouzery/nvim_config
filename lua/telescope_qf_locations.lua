d77c23e07d9007279e88f5deb2a1424739d293f8
Emmanuel Touzery <etouzery@gmail.com>
2025-01-25 13:00:30 +0100

    better entry display for telescope QF, incl highlighting

--- lua/telescope_qf_locations.lua
+++ lua/telescope_qf_locations.lua
@@ -5,13 +5,14 @@ local entry_display = require "telescope.pickers.entry_display"
 local conf = require("telescope.config").values
 
 function gen_from_quickfix(opts)
+  local quicker_hl = require'quicker.highlight'
   opts = opts or {}
 
   local displayer = entry_display.create {
     separator = "▏",
     items = {
       { width = 5 },
-      { width = 0.45 },
+      { width = 22 },
       { remaining = true },
     },
   }
@@ -25,12 +26,24 @@ function gen_from_quickfix(opts)
       entry.text = entry.text:gsub("^%s*(.-)%s*$", "%1")
     end
 
-    return displayer {
+    local d = displayer {
       line_info,
-      entry.text:gsub(".* | ", ""),
       -- filename,
       filename:match("[^/]+$"),
+      entry.text:gsub(".* | ", ""),
     }
+
+    if entry.bufnr and vim.api.nvim_buf_is_loaded(entry.bufnr) and entry.lnum ~= nil then
+      local hl = quicker_hl.buf_get_ts_highlights(entry.bufnr, entry.lnum)
+      local offset = 5+22+6 -- col1+col2+?
+      local hl2 = {}
+      for _, seh in ipairs(hl) do
+        table.insert(hl2, {{seh[1]+offset, seh[2]+offset}, seh[3]})
+      end
+      return d, hl2
+    else
+      return d
+    end
   end
 
   return function(entry)

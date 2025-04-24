45c9274f112ac456a84643d5b1b030a91a3d3331
Emmanuel Touzery <etouzery@gmail.com>
2025-04-24 08:56:09 +0200

    quick set ft, add csv

--- lua/leader_shortcuts.lua
+++ lua/leader_shortcuts.lua
@@ -67,7 +67,10 @@ vim.keymap.set("n", "<leader>fdc", ":lua open_file_cur_dir(true)<cr>", {desc="op
 vim.keymap.set('n', '<leader>f!', ":lua reload_all()<cr>", {desc = "Reload all files from disk"})
 
 function _G.quick_set_ft()
-  local filetypes = {"typescript", "json", "elixir", "rust", "lua", "diff", "sh", "markdown", "html", "config", "sql", "strace", "xml", "other", "logcat", "astgrep"}
+  local filetypes = {
+    "typescript", "json", "elixir", "rust", "lua", "diff", "sh", "markdown", "html", "config",
+    "sql", "strace", "xml", "other", "logcat", "astgrep", "csv",
+  }
   vim.ui.select(filetypes, {prompt="Pick filetype to switch to"}, function(choice)
     if choice == "other" then
       vim.ui.input({prompt="Enter filetype", kind="center_win"}, function(word)

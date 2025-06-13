b08383f3c68a8a6c9d2bedbc78ac1f510a5e48ea
Emmanuel Touzery <etouzery@gmail.com>
2025-06-13 22:07:40 +0200

    first version of a shortcut to switch between redux selector params
    not really finished and messy but good enough for a first commit of
    this.

--- ftplugin/typescript.lua
+++ ftplugin/typescript.lua
@@ -64,3 +64,78 @@ end
 -- tool for exploratory type programming. Extract a type T that can be explored with the LSP hover, K
 vim.keymap.set('n', '<localleader>t', extract_type, { buffer = true, desc = "Extract type for K exploration" })
 
+local function jump_param()
+  local parser = require('nvim-treesitter.parsers').get_parser(0)
+  parser:parse()
+  local ts_node = parser:named_node_for_range({vim.fn.line('.')-1, vim.fn.col('.')-1, vim.fn.line('.')-1, vim.fn.col('.')-1})
+  local parent = ts_node
+  local required_param = nil
+  local is_final_selector = nil
+  local param_idx = nil
+  while parent do
+    if parent:type() == "call_expression" then
+      break
+    elseif parent:type() == "required_parameter" then
+      required_param = parent
+    elseif parent:type() == "arrow_function" then
+      -- were we in the injected params or the params of the final selector?
+      is_final_selector = parent:next_named_sibling() == nil
+      if not is_final_selector then
+        local cur_param = parent:prev_named_sibling()
+        param_idx = 1
+        while cur_param ~= nil do
+          cur_param = cur_param:prev_named_sibling()
+          param_idx = param_idx + 1
+        end
+        print("Param index is " .. param_idx)
+      end
+    end
+    local p = parent:parent()
+    parent = p
+  end
+  local fn_identifier = vim.treesitter.get_node_text(parent:named_child(0), 0)
+  if fn_identifier == "createSelector" then
+  else
+    notif({"Not a redux selector param"})
+  end
+  if is_final_selector then
+    -- which param index are we?
+    local cur_param = required_param:prev_named_sibling()
+    local param_idx = 1
+    while cur_param ~= nil do
+      cur_param = cur_param:prev_named_sibling()
+      param_idx = param_idx + 1
+    end
+    print("Param index is " .. param_idx)
+
+    -- go to target
+    -- i would like to say named_child(param_idx) but for instance
+    -- if there are comments, they are named nodes too and will
+    -- shift parameters. rather count the nodes of the arrow_function type.
+    local target = parent:named_child(1):named_child(0)
+    local i = 0
+    while i < param_idx do
+      if target:type() == "arrow_function" then
+        i = i + 1
+      end
+      target = target:next_named_sibling()
+    end
+    local start_row, _, _, _ = target:range(target)
+    vim.cmd("normal! m'") -- add to jump list
+    vim.cmd(":" .. start_row)
+  else
+    -- go to target
+    local params = parent:named_child(1)
+    local selector_callback = params:named_child(params:named_child_count()-1)
+    print(selector_callback:type())
+    print(selector_callback:named_child(0):type())
+    -- TODO could be comments or other nodes. should make a loop
+    -- like with the other go to target
+    local param = selector_callback:named_child(0):named_child(param_idx)
+    local start_row, _, _, _ = param:range()
+    vim.cmd("normal! m'") -- add to jump list
+    vim.cmd(":" .. start_row)
+  end
+end
+
+vim.keymap.set('n', '<localleader>jp', jump_param, { buffer = true, desc = "redux selector jump to matching param"})

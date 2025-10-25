dd12dad78f7663d7851cd26adea93808acc18995
Emmanuel Touzery <etouzery@gmail.com>
2025-10-25 18:33:19 +0200

    surround for TSX generic tags
    https://github.com/nvim-mini/mini.nvim/discussions/2079

--- ftplugin/typescriptreact.lua
+++ ftplugin/typescriptreact.lua
@@ -1,2 +1,19 @@
 -- https://www.reddit.com/r/neovim/comments/ykpnob/comment/iuy3lea/
 vim.cmd.runtime({"ftplugin/typescript.lua",  bang = true })
+
+-- Set specific surrounding in 'mini.surround'
+local ts_input = require('mini.surround').gen_spec.input.treesitter
+vim.b.minisurround_config = {
+  custom_surroundings = {
+    t = {
+      input = ts_input({ outer = '@tag.outer', inner = '@tag.inner' }),
+    },
+    T = {
+      input = ts_input({ outer = '@tag_name.outer', inner = '@tag_name.inner' }),
+      output = function()
+        local tag_name = MiniSurround.user_input('Tag name')
+        return { left = tag_name, right = tag_name }
+      end,
+    },
+  },
+}

07d85e79a197937a52ef27125eb0fbe09c94c4eb
Emmanuel Touzery <etouzery@gmail.com>
2026-05-27 20:11:38 +0200

    more telescope_buffers_entry_maker guards...

--- lua/telescope_buffers_entry_maker.lua
+++ lua/telescope_buffers_entry_maker.lua
@@ -16,9 +16,9 @@ local function display_changes(entry)
     return "-"
   end
   local t = {}
-  if summary.add > 0 then table.insert(t, '+' .. summary.add) end
-  if summary.change > 0 then table.insert(t, '~' .. summary.change) end
-  if summary.delete > 0 then table.insert(t, '-' .. summary.delete) end
+  if summary.add and summary.add > 0 then table.insert(t, '+' .. summary.add) end
+  if summary.change and summary.change > 0 then table.insert(t, '~' .. summary.change) end
+  if summary.delete and summary.delete > 0 then table.insert(t, '-' .. summary.delete) end
   return table.concat(t, '')
 end
 

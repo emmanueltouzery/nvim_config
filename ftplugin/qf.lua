3da1dbddeb5c2ab98592938f2cd98378d7461570
Emmanuel Touzery <etouzery@gmail.com>
2026-06-16 20:06:01 +0200

    quickfix: ability to remove the entry under the cursor
    useful when treating the qf as a "todo" list

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -5,3 +5,14 @@ local function clear_qf()
 end
 
 vim.keymap.set('n', '<localleader>x', clear_qf, { buffer = true, desc = "Clear the quickfix list" })
+
+local function remove_qf_under_cursor()
+  local qf = vim.fn.getqflist()
+  table.remove(qf, vim.fn.line('.'))
+  vim.fn.setqflist(qf, 'u')
+  require('quicker').refresh()
+  vim.cmd[[MarkifyClear]]
+  vim.cmd[[Markify]]
+end
+
+vim.keymap.set('n', '<localleader>d', remove_qf_under_cursor, { buffer = true, desc = "Remove QF entry under cursor" })

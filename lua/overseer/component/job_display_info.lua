2768f6e2cad44ddaec08b5a80ef231bac4803363
Emmanuel Touzery <etouzery@gmail.com>
2026-08-13 16:33:50 +0200

    fix job_display_info progress integration

--- lua/overseer/component/job_display_info.lua
+++ lua/overseer/component/job_display_info.lua
@@ -6,21 +6,23 @@ return {
     }
   },
   constructor = function(params)
+    local msg_id = nil
     return {
       on_start = function(self, task)
-        local msg_id = vim.api.nvim_echo({{params.message}}, true, {
+        msg_id = vim.api.nvim_echo({{params.message}}, true, {
           kind='progress', status='running', title=params.message, source='Job status', percent = nil
         })
       end,
       on_reset = function(self, task)
-        local msg_id = vim.api.nvim_echo({{params.message}}, true, {
+        msg_id = vim.api.nvim_echo({{params.message}}, true, {
           kind='progress', status='running', title=params.message, source='Job status', percent = nil
         })
       end,
       on_exit = function(self, task, status)
         vim.api.nvim_echo({{'Done'}}, true, {
-          id = msg_id, kind='progress', status=status, source='Job status', title=params.message
+          id = msg_id, kind='progress', status='success', source='Job status', title=params.message
         })
+        msg_id = nil
       end,
     }
   end,

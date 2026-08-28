3f518b9a31c614ae39e268486f62513d09849b19
Emmanuel Touzery <etouzery@gmail.com>
2026-08-28 20:21:00 +0200

    fix progress bar display for overseer job_display_info

--- lua/overseer/component/job_display_info.lua
+++ lua/overseer/component/job_display_info.lua
@@ -19,9 +19,6 @@ return {
             id = msg_id, kind='progress', status='success', source='Job status', title=params.message
           })
         end
-        msg_id = vim.api.nvim_echo({{params.message}}, true, {
-          kind='progress', status='running', title=params.message, source='Job status', percent = nil
-        })
       end,
       on_exit = function(self, task, status)
         vim.api.nvim_echo({{'Done'}}, true, {
